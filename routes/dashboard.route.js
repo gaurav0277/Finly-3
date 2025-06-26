c3074bd09fbecd0c193bb7fa718161c3fe79cb09
gaurav0277 <gauravkumarpal9368@gmail.com>
2025-06-26 10:28:23 +0530

    second commit

--- routes/dashboard.route.js
+++ routes/dashboard.route.js
@@ -1,14 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const customersRouter = require('./customer.route');
-const invoicesRouter = require('./invoice.route');
+const customersRouter = require("./customer.route");
 
-const { showDashboard } = require('../controllers/dashboard.controller');
+const { showDashboard } = require("../controllers/dashboard.controller");
 
-router.get('/', showDashboard);
+router.get("/", showDashboard);
 
-router.use('/customers', customersRouter);
-router.use('/invoices', invoicesRouter);
+router.use("/customers", customersRouter);
 
 module.exports = router;
